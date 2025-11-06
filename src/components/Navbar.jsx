1ea28fb3ea754a81d8c2b31a3a1f3e4250ed4c8a
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-11-06 01:53:18 +0000

    navbar en proceso con boton de favoritos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
 
+	const {store, dispatch} = useGlobalReducer()
+	
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={store.logotipo} style={{height:"75px"}}/>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="#">
+						<button className="btn btn-danger rounded-5" style={{width: "190px"}}>Favoritos</button>
 					</Link>
 				</div>
 			</div>
